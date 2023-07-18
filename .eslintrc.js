49703c454fedb585b5a06ad052de729f0c55b1f3
Marcos Coelho <mparav124@gmail.com>
2023-07-18 13:47:06 -0300

    add: arquivo de configuração eslint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'next',
+  ],
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    quotes: [2, 'single', { avoidEscape: true }],
+    indent: [2, 4],
+    semi: [2, 'always'],
+    'no-console': 2,
+  },
+};
