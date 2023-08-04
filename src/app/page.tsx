fe5e1c5edbca89a03ab18d4b03d4ea7a19eaf9c0
Diogo Almeida <diogo.g07@icloud.com>
2023-08-04 08:38:18 -0300

    add-analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Footer from "@/components/Home/Footer/Footer";
 import Header from "@/components/Home/Header/Header";
 import Heading from "@/components/Home/Heading/Heading";
 import HomeForm from "@/components/Home/HomeForm/HomeForm";
+import { Analytics } from '@vercel/analytics/react';
 
 const HomePage = () => {
   return (
@@ -16,6 +17,7 @@ const HomePage = () => {
         <Competencies/>
         <HomeForm/>
         <Contact/>
+        <Analytics />
       </main>
       <Footer/>
     </>
