983205dd4c38117790f16a43c2f695432c545903
vinisooo <cviniciusliramdev@gmail.com>
2023-08-08 13:04:22 -0300

    style: adjust login form style

--- src/components/Inputs/Input.tsx
+++ src/components/Inputs/Input.tsx
@@ -1,6 +1,7 @@
 import React, { ChangeEvent, useState } from 'react';
 import { AiOutlineEye } from 'react-icons/ai';
 import { BsEyeFill, BsEyeSlash, BsEyeSlashFill } from 'react-icons/bs';
+import { PiEyeBold, PiEyeSlashBold } from 'react-icons/pi';
 
 interface IInputProps {
   label: string;
@@ -25,15 +26,17 @@ const Input = ({ label, id, type, setValue, value, placeholder }: IInputProps) =
   return (
     <div className='flex flex-col gap-2'>
       <label className='font-default-font text-sm uppercase font-semibold text-grey-100' htmlFor={id}>{label}</label>
-      <div className='rounded-lg bg-grey-50 px-3 py-2 flex items-center hover:border-1 hover:border-brand-100'>
-        <input placeholder={placeholder} className='w-full bg-transparent outline-0' type={inputType} id={id} name={id} onChange={handleChange} value={value}  />
+      <div className='rounded-lg bg-grey-50 px-3 flex items-center border-grey-50 hover:border-1 transition-all duration-200
+      hover:border-brand-200 hover:border-solid border-[3px]'>
+        <input placeholder={placeholder} className='w-full py-4 bg-transparent outline-0' type={inputType} id={id} name={id} onChange={handleChange} value={value}  />
         <button type="button" onClick={handlePasswordType}>
-          {type === 'password' &&
+          {
+          type === 'password' &&
             (
               inputType === "password" ?
-              <BsEyeFill className={`scale-125 hover:text-brand-200 hover:cursor-pointer text-brand-300`}/> 
+              <PiEyeBold className={`scale-125 hover:text-brand-200 hover:cursor-pointer text-brand-300`}/> 
               :
-              <BsEyeSlashFill className={`scale-125 hover:text-brand-200 hover:cursor-pointer text-grey-200`}/> 
+              <PiEyeSlashBold className={`scale-125 hover:text-brand-200 hover:cursor-pointer text-grey-200`}/> 
             )
           }
         </button>
--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -14,22 +14,22 @@ const LoginPage = () => {
 
   return (
       <FormPage>
-        <section className='bg-white-primary w-full py-12 px-6  sm:rounded-xl sm:max-w-xl'>
+        <section className='bg-white-primary w-full py-12 px-6  sm:rounded-xl max-w-lg mx-5'>
           <header className='flex flex-col items-center gap-5 mb-10'>
             <Image src={logoBlue} alt='Logo SwiftStock'/>
-            <p className='font-default-font text-grey-100 text-base text-center'>Lorem ipsum dolor amet lorem ipsum</p>
+            <p className='font-default-font text-grey-100 text-sm text-center'>Lorem ipsum dolor amet lorem ipsum</p>
           </header>
 
           <form className='flex flex-col gap-5 mb-8' action="">
             <Input type='text' id='email' label='E-mail' placeholder='Insira seu Email' value={email} setValue={setEmail} />
             <Input type='password' id='email' placeholder='Insira sua Senha' label='Password' value={password} setValue={setPassword} />
 
-            <Link href={'#'} className='py-2 rounded-lg uppercase font-default-font text-center text-base text-white-primary font-bold bg-brand-200'>Acessar</Link>
+            <button className='py-4 rounded-lg uppercase font-default-font text-center text-base text-white-primary font-bold bg-brand-200'>Acessar</button>
             <Link className='text-xs text-right underline text-brand-100 font-semibold font-default-font' href={'#'}>Esqueci minha senha</Link>
           </form>
 
           <footer className='flex justify-center '>
-            <Link href={'#'} className='py-2 rounded-lg w-full bg-brand-50 text-brand-100 uppercase text-center font-default-font font-bold'>Criar conta</Link>
+            <Link href={'#'} className='py-4 rounded-lg w-full bg-brand-50 text-brand-100 uppercase text-center font-default-font font-bold'>Criar conta</Link>
           </footer>
         </section>
       </FormPage>
