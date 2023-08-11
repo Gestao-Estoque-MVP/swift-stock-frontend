3ec690f473d95886fed7d11e9c7a2fd1e9f830b4
Diogo Almeida <diogo.g07@icloud.com>
2023-08-11 08:56:45 -0300

    melhoria

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Footer from "@/components/Home/Footer/Footer";
 import Header from "@/components/Home/Header/Header";
 import Heading from "@/components/Home/Heading/Heading";
 import HomeForm from "@/components/Home/HomeForm/HomeForm";
-import Analytics from '@vercel/analytics/react';
+import {Analytics} from '@vercel/analytics/react';
 
 const HomePage = () => {
   return (
