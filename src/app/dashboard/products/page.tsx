fc2051cfddf02f450d0c1168e8c865d01f3b4cad
gustavocodigo <gilapesgus@gmail.com>
2023-08-16 20:47:55 -0300

    Added cards

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -1,12 +1,52 @@
 import { Table } from "@/components/Table"
 
+import ProductCard from "../../../components/ProductCard"
+
 const Products = () => {
+
+
+    const itens = []
+
+    for (let i = 1; i <= 100; i++) {
+
+        let item = {
+            infor: { name: "Item Name " + i, imageUrl: "https://avatars.githubusercontent.com/u/139462945?s=200&v=4" },
+            categoria: `Acessório ${i}`,
+            entrada: "10/07/2023",
+            sku: `0000${i % 9}`,
+            qtd: i * 100,
+            stock: i * 10,
+            estoque: "completo"
+        }
+        if ( i == 2 ){item.estoque="vazio"};
+        if ( i == 5 ){item.estoque="baixo"};
+
+        itens.push(item);
+    }
+
+
+
     return (
         <>
-        <h1 className="text-brand-400">Dashboard Page</h1>
-        <Table />
+            <div className="p-24 max-w-[1600px] mx-auto ">
+                <h1 className="text-brand-400">Dashboard Page</h1>
+                <div className="flex space-x-8 justify-between">
+                    <ProductCard productCount={1000} bad={false} />
+                    <ProductCard productCount={4000} bad={false} />
+                    <ProductCard productCount={100} bad={true} />
+                    <ProductCard productCount={2000} bad={false} />
+
+                </div>
+
+                <div className="mt-[38px]">
+                    <Table itens={itens} />
+                </div>
+            </div>
+
+
+
         </>
     )
 }
-  
+
 export default Products
--- src/components/Table/ProductStatus.tsx
+++ src/components/Table/ProductStatus.tsx
@@ -1,14 +1,39 @@
 import React from 'react';
 
 interface IFormPageProps {
-  children: React.ReactNode
+  children: React.ReactNode,
+  state: string,
 }
 
-const ProductStatus = ({children}: IFormPageProps) => {
+
+
+
+const states = {
+  "completo": { color: "#3026DC", bg: "#DFEDF2",text:"Completo" },
+  "baixo": { color: "#FFAB2D", bg: "rgba(255, 235, 204, 0.50)", text:"Baixo" },
+  "vazio": { color: "#FF4A55", bg: "gba(255, 74, 85, 0.25)",text:"Vazio" },
+  "medio": { color: "#36C633", bg: "#EBFCEF",text:"Médio"},
+}
+
+
+
+
+const ProductStatus = ({ children, state }: IFormPageProps) => {
   return (
-    <div>
-        {children}
-    </div>
+
+    <button className='p-[14px] flex items-center gap-2' style={{
+      color: states[state] ? states[state].color : "white",
+      backgroundColor: states[state] ? states[state].bg : "red",
+      fontSize: "16px",
+      fontWeight: "500"
+
+    }}>
+      <svg width="8" height="9" viewBox="0 0 8 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="4" cy="4.5" r="4" fill="currentColor" />
+      </svg>
+      {(states[state] ? states[state].text : "Desconhecido")}
+    </button>
+
   )
 }
 
