01f4f925e8f2676d0f0562904eab759d3c179fa1
diogoX451 <diogo.g07@icloud.com>
2023-08-28 00:22:00 -0300

    melhoria

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,34 @@
+<<<<<<< HEAD
 
 const DashboardPage = () => {
     return <h1 className="text-brand-400">Dashboard Page</h1>
+=======
+'use client'
+import Layout from "@/components/Layout/input";
+import Products from "./products/page";
+import { useState } from 'react';
+
+
+const DashboardPage = () => {
+    
+    const [activeContent, setActiveContent] = useState('');
+    const renderContent = () => {
+        switch (activeContent) {
+            case 'products':
+                return <Products />;
+            case 'dashboard':
+                return <></>;
+            default:
+                return <></>;
+        }
+    };
+    
+    return (
+        <Layout onMenuClick={setActiveContent}>
+            {renderContent()}
+        </Layout>
+    )
+>>>>>>> bfdae41 (melhoria)
 }
   
 export default DashboardPage
